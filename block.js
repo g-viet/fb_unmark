4c5211a9903cc6b97399fa6f5db54ba9f9479b1f
vietkungfu <vietbktn@gmail.com>
2017-04-16 23:53:09 +0900

    block last active

--- block.js
+++ block.js
@@ -5,11 +5,15 @@ chrome.webRequest.onBeforeRequest.addListener(
       details.url.includes("ajax/notifications/mark_read") ||
       details.url.includes("ajax/gigaboxx/endpoint/UpdateLastSeenTime") ||
       details.url.includes("reconnect") ||
-      details.url.includes("friendjewel/friendconfirmednotifications")
+      details.url.includes("friendjewel/friendconfirmednotifications") ||
+      details.url.includes("active_ping")
       )
     {
       console.log("So in");
       return {cancel: true}
+    } else if (details.url.includes(".com/pull")) {
+      var replaceUrl = details.url.replace(/active/i, 'offline');
+      return {redirectUrl: replaceUrl};
     }
   },
   {urls: ["*://*.facebook.com/*", "*://*.messenger.com/*"]},
