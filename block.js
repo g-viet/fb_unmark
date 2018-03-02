1bca411e4fb8d89a40aa0b8ce4bb4bc89f4fe7ed
Viet <g-viet@eole.co.jp>
2018-03-02 17:47:28 +0900

    trigger unmark feature by using popup

--- block.js
+++ block.js
@@ -1,25 +1,55 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    if(details.url.includes("ajax/mercury/mark_seen") ||
-      details.url.includes("ajax/notifications/mark_read") ||
-      details.url.includes("ajax/gigaboxx/endpoint/UpdateLastSeenTime") ||
-      details.url.includes("reconnect") ||
-      details.url.includes("friendjewel/friendconfirmednotifications") ||
-      details.url.includes("active_ping") ||
-      details.url.includes("mercury/change_read_status.php")
-      )
-    {
-      var a;
-      chrome.storage.local.get(["offline"], function(items){
-        a = items;
-      });
-      console.log(a);
-      return {cancel: true}
-    } else if (details.url.includes(".com/pull")) {
-      var replaceUrl = details.url.replace(/active/i, 'offline');
-      return {redirectUrl: replaceUrl};
+
+var filterFbRequestCallback = function (details) {
+  if(details.url.includes("ajax/mercury/mark_seen") ||
+    details.url.includes("ajax/notifications/mark_read") ||
+    details.url.includes("ajax/gigaboxx/endpoint/UpdateLastSeenTime") ||
+    details.url.includes("reconnect") ||
+    details.url.includes("friendjewel/friendconfirmednotifications") ||
+    details.url.includes("active_ping") ||
+    details.url.includes("mercury/change_read_status.php")
+    )
+  {
+    return {cancel: true}
+  } else if (details.url.includes(".com/pull")) {
+    var replaceUrl = details.url.replace(/active/i, 'offline');
+    return {redirectUrl: replaceUrl};
+  }
+};
+
+var filter = {urls: ["*://*.facebook.com/*", "*://*.messenger.com/*"]};
+
+var opt_extraInfoSpec = ["blocking"];
+
+chrome.storage.onChanged.addListener(function(changes, areaName){
+  if (areaName == "local") {
+    if (changes.offline.newValue) {
+      chrome.webRequest.onBeforeRequest.addListener(
+        filterFbRequestCallback,
+        filter,
+        opt_extraInfoSpec
+      );
+    } else {
+      chrome.webRequest.onBeforeRequest.removeListener(
+        filterFbRequestCallback,
+        filter,
+        opt_extraInfoSpec
+      );
     }
-  },
-  {urls: ["*://*.facebook.com/*", "*://*.messenger.com/*"]},
-  ["blocking"]
-);
+  }
+});
+
+chrome.storage.local.get(["offline"], function(items){
+  if (items.offline) {
+    chrome.webRequest.onBeforeRequest.addListener(
+      filterFbRequestCallback,
+      filter,
+      opt_extraInfoSpec
+    );
+  } else {
+    chrome.webRequest.onBeforeRequest.removeListener(
+      filterFbRequestCallback,
+      filter,
+      opt_extraInfoSpec
+    );
+  }
+});
--- popup.js
+++ popup.js
@@ -1,15 +1,7 @@
+chrome.storage.local.get(["offline"], function(items){
+  document.getElementById('offline_settings').checked =	items.offline || false;
+});
+
 document.getElementById('offline_settings').addEventListener('change', function(e){
-	if (e.target.checked){
-		chrome.storage.local.set({ "offline": true }, function(){});
-		console.log("is offline");
-		chrome.storage.local.get(["offline"], function(items){
-	        console.log(items);
-	      });
-	}else {
-		chrome.storage.local.set({ "offline": false }, function(){});
-		console.log("is not offline");
-		chrome.storage.local.get(["offline"], function(items){
-	        console.log(items);
-	      });
-	}
+  chrome.storage.local.set({ "offline": e.target.checked }, function(){});
 })
